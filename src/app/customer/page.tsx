69f0162ace032c25b66ce2c5a68532316355ef33
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-04-06 16:45:16 +0530

    u

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -480,6 +480,7 @@ export default function Customer() {
       getCustomers();
       fetchFields();
       getTotalCustomerPage();
+      setCustomerTableLoader(false);
     }
     fetchAiAgents();
     fetchTodayCustomer();
